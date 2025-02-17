3ce17d3f0c96b6272a9bccefd87bae30096655a9
YohanAbeysinghe <abeysingheyohan@gmail.com>
2025-02-17 17:08:08 +0200

    num gpus

--- finetune/finetune_multigpu.py
+++ finetune/finetune_multigpu.py
@@ -70,8 +70,7 @@ local_rank = setup_distributed()
 device = torch.device(f"cuda:{local_rank}")
 logger.info(f"Using device: {device}")
 
-# num_gpus = torch.cuda.device_count()
-num_gpus = 4
+num_gpus = torch.cuda.device_count()
 if local_rank == 0:
     logger.info(f"Number of GPUs available: {num_gpus}")
 #
